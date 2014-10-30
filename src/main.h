100be82393e3ce6795e5cbdb449f710612e5e475
arturalkaim <arturalkaim@gmail.com>
2014-10-30 20:40:54 +0100

    adding main.h

---
+++ src/main.h
@@ -0,0 +1,15 @@
+//
+//  main.h
+//  Test1
+//
+//  Created by Artur Alkaim on 30/10/14.
+//  Copyright (c) 2014 Artur Alkaim. All rights reserved.
+//
+
+#ifndef Test1_main_h
+#define Test1_main_h
+
+int N = 10, M = 5;
+double ** mat ;
+
+#endif
