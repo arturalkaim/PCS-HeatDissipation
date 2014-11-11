31367df77233ce58dd6ae7ddce4bb1bd20f13393
RafaelReia <rafael.reia@gmail.com>
2014-11-11 11:11:22 +0100

    mergesort updated

--- src/mergeSortSeq.c
+++ src/mergeSortSeq.c
@@ -1,36 +1,16 @@
-TopDownMergeSort(A[], B[], n)
-{
-    TopDownSplitMerge(A, 0, n, B);
-}
+//#include <omp.h>
 
-CopyArray(B[], iBegin, iEnd, A[])
-{
-    for(int k = iBegin; k < iEnd; k++)
-        A[k] = B[k];
-}
-
-// iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set)
-TopDownSplitMerge(A[], iBegin, iEnd, B[])
-{
-    if(iEnd - iBegin < 2)                       // if run size == 1
-        return;                                 //   consider it sorted
-    // recursively split runs into two halves until run size == 1,
-    // then merge them and return back up the call chain
-    iMiddle = (iEnd + iBegin) / 2;              // iMiddle = mid point
-    TopDownSplitMerge(A, iBegin,  iMiddle, B);  // split / merge left  half
-    TopDownSplitMerge(A, iMiddle,    iEnd, B);  // split / merge right half
-    TopDownMerge(A, iBegin, iMiddle, iEnd, B);  // merge the two half runs
-    CopyArray(B, iBegin, iEnd, A);              // copy the merged runs back to A
-}
 
 //  left half is A[iBegin :iMiddle-1]
 // right half is A[iMiddle:iEnd-1   ]
-TopDownMerge(A[], iBegin, iMiddle, iEnd, B[])
+void TopDownMerge(int* A, int iBegin, int iMiddle, int iEnd, int* B)
 {
-    i0 = iBegin, i1 = iMiddle;
+    int i0 = iBegin, i1 = iMiddle;
 
     // While there are elements in the left or right runs
-    for (int j = iBegin; j < iEnd; j++) {
+    int j;
+//#pragma omp parallel for firstprivate(i0, i1)
+    for (j = iBegin; j < iEnd; j++) {
         // If left run head exists and is <= existing right run head.
         if (i0 < iMiddle && (i1 >= iEnd || A[i0] <= A[i1])){
             B[j] = A[i0];
@@ -42,3 +22,58 @@ TopDownMerge(A[], iBegin, iMiddle, iEnd, B[])
     }
 
 }
+
+void CopyArray(int* B, int iBegin, int iEnd,int* A)
+{
+	int k;
+    for(k = iBegin; k < iEnd; k++)
+        A[k] = B[k];
+}
+
+
+void TopDownSplitMergeNew(int* A, int iBegin, int iEnd, int* B){
+	if(iEnd - iBegin < 2)                       // if run size == 1
+	        return;                                 //   consider it sorted
+	    // recursively split runs into two halves until run size == 1,
+	    // then merge them and return back up the call chain
+	    int iMiddle = (iEnd + iBegin) / 2;              // iMiddle = mid point, it keeps the lower, doesn't round up
+	    TopDownSplitMergeNew(A, iBegin,  iMiddle, B);  // split / merge left  half
+	    TopDownSplitMergeNew(A, iMiddle,    iEnd, B);  // split / merge right half
+	    TopDownMerge(A, iBegin, iMiddle, iEnd, B);  // merge the two half runs
+	    CopyArray(B, iBegin, iEnd, A);              // copy the merged runs back to A
+}
+
+// iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set)
+void TopDownSplitMerge(int* A, int iBegin, int iEnd, int* B)
+{
+    if(iEnd - iBegin < 2)                       // if run size == 1
+        return;                                 //   consider it sorted
+    // recursively split runs into two halves until run size == 1,
+    // then merge them and return back up the call chain
+    int iMiddle = (iEnd + iBegin) / 2;              // iMiddle = mid point, it keeps the lower, doesn't round up
+	#pragma omp parallel sections
+    {
+    	//printf("%d \n", omp_get_num_threads());
+		#pragma omp section
+    	{
+    TopDownSplitMergeNew(A, iBegin,  iMiddle, B);  // split / merge left  half
+    	}
+		#pragma omp section
+    	{
+    TopDownSplitMergeNew(A, iMiddle,    iEnd, B);  // split / merge right half
+    	}
+    }
+
+    TopDownMerge(A, iBegin, iMiddle, iEnd, B);  // merge the two half runs
+    CopyArray(B, iBegin, iEnd, A);              // copy the merged runs back to A
+}
+
+
+void TopDownMergeSort(int* A,int* B,int n)
+{
+
+    TopDownSplitMerge(A, 0, n, B);
+}
+
+
+
